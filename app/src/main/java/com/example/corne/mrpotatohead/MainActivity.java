6c21bafc6ac2ec74fede8d67cb3757f7f63ddc8a
Corne <39339251+corne12345@users.noreply.github.com>
2018-12-12 20:52:47 +0100

    Cleaned code, app works perfectly

--- app/src/main/java/com/example/corne/mrpotatohead/MainActivity.java
+++ app/src/main/java/com/example/corne/mrpotatohead/MainActivity.java
@@ -7,136 +7,71 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    // Global variables of type ImageView to prevent repetitive appointment
-    public ImageView armsIV, earsIV, eyebrowsIV, eyesIV, glassesIV, hatIV, mouthIV, mustacheIV, noseIV, shoesIV;
+    // Global list of names of all the items (both in text as in ImageView id's)
+    public String itemNames[] = {"Arms", "Ears", "Eyebrows", "Eyes", "Glasses", "Hat", "Mouth", "Mustache", "Nose", "Shoes"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Load the states of all the images if there is an instance present
+        // Load states if present
         if (savedInstanceState != null) {
-            (findViewById(R.id.Arms)).setVisibility(savedInstanceState.getInt("arms"));
-            (findViewById(R.id.Ears)).setVisibility(savedInstanceState.getInt("ears"));
-            (findViewById(R.id.Eyebrows)).setVisibility(savedInstanceState.getInt("eyebrows"));
-            (findViewById(R.id.Eyes)).setVisibility(savedInstanceState.getInt("eyes"));
-            (findViewById(R.id.Glasses)).setVisibility(savedInstanceState.getInt("glasses"));
-            (findViewById(R.id.Hat)).setVisibility(savedInstanceState.getInt("hat"));
-            (findViewById(R.id.Mouth)).setVisibility(savedInstanceState.getInt("mouth"));
-            (findViewById(R.id.Mustache)).setVisibility(savedInstanceState.getInt("mustache"));
-            (findViewById(R.id.Nose)).setVisibility(savedInstanceState.getInt("nose"));
-            (findViewById(R.id.Shoes)).setVisibility(savedInstanceState.getInt("shoes"));
+
+            // Loop over the list of item names
+            for (int i = 0; i < itemNames.length; i++) {
+
+                // Get R.id of every item and set its visibility according to saved state
+                int resID = getResources().getIdentifier(itemNames[i], "id", getPackageName());
+                (findViewById(resID)).setVisibility(savedInstanceState.getInt(itemNames[i]));
+            }
         }
     }
+
     @Override
-    public void onSaveInstanceState(Bundle outState){
+    public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        // Only save visibility since checkboxes are automatically saved
-        outState.putInt("arms", (findViewById(R.id.Arms)).getVisibility());
-        outState.putInt("ears", (findViewById(R.id.Ears)).getVisibility());
-        outState.putInt("eyebrows", (findViewById(R.id.Eyebrows)).getVisibility());
-        outState.putInt("eyes", (findViewById(R.id.Eyes)).getVisibility());
-        outState.putInt("glasses", (findViewById(R.id.Glasses)).getVisibility());
-        outState.putInt("hat", (findViewById(R.id.Hat)).getVisibility());
-        outState.putInt("mouth", (findViewById(R.id.Mouth)).getVisibility());
-        outState.putInt("mustache", (findViewById(R.id.Mustache)).getVisibility());
-        outState.putInt("nose", (findViewById(R.id.Nose)).getVisibility());
-        outState.putInt("shoes", (findViewById(R.id.Shoes)).getVisibility());
+        // Loop over the list of all the items
+        for (int i = 0; i < itemNames.length; i++){
+
+            // Get all Rid's of imageviews and save its visibility
+            int resID = getResources().getIdentifier(itemNames[i], "id", getPackageName());
+            outState.putInt(itemNames[i], (findViewById(resID)).getVisibility());
+        }
     }
 
+    // Method that is activated, when a checkbox is clicked
     public void checkClicked(View v) {
+
+        // collect variables of this instance for further use
         CheckBox checkbox = (CheckBox) v;
         String clothing = checkbox.getText().toString();
         boolean checked = checkbox.isChecked();
 
-        // Create switch statement to assign all Imageviews and set visibility
-        switch (clothing) {
-            case "Arms":
-                armsIV = findViewById(R.id.Arms);
+        // Loop over all the bodypart names
+        for (int i = 0; i < itemNames.length; i++) {
+
+            // When a match is found between bodypart and checked box
+            if (itemNames[i].equals(clothing)) {
+
+                // Match checkbox to imagaview of this bodypart
+                int resID = getResources().getIdentifier(itemNames[i], "id", getPackageName());
+                ImageView clickedItem = (ImageView) findViewById(resID);
+
+                // Change visibility basedd on whether or not box is checked
                 if (checked) {
-                    armsIV.setVisibility(View.VISIBLE);
+                    clickedItem.setVisibility(View.VISIBLE);
                 } else {
-                    armsIV.setVisibility(View.INVISIBLE);
+                    clickedItem.setVisibility(View.INVISIBLE);
                 }
-                break;
-            case "Ears":
-                earsIV = findViewById(R.id.Ears);
-                if (checked) {
-                    earsIV.setVisibility(View.VISIBLE);
-                } else {
-                    earsIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Eyebrows":
-                eyebrowsIV = findViewById(R.id.Eyebrows);
-                if (checked) {
-                    eyebrowsIV.setVisibility(View.VISIBLE);
-                } else {
-                    eyebrowsIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Eyes":
-                eyesIV = findViewById(R.id.Eyes);
-                if (checked) {
-                    eyesIV.setVisibility(View.VISIBLE);
-                } else {
-                    eyesIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Glasses":
-                glassesIV = findViewById(R.id.Glasses);
-                if (checked) {
-                    glassesIV.setVisibility(View.VISIBLE);
-                } else {
-                    glassesIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Hat":
-                hatIV = findViewById(R.id.Hat);
-                if (checked) {
-                    hatIV.setVisibility(View.VISIBLE);
-                } else {
-                    hatIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Mouth":
-                mouthIV = findViewById(R.id.Mouth);
-                if (checked) {
-                    mouthIV.setVisibility(View.VISIBLE);
-                } else {
-                    mouthIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Mustache":
-                mustacheIV = findViewById(R.id.Mustache);
-                if (checked) {
-                    mustacheIV.setVisibility(View.VISIBLE);
-                } else {
-                    mustacheIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Nose":
-                noseIV = findViewById(R.id.Nose);
-                if (checked) {
-                    noseIV.setVisibility(View.VISIBLE);
-                } else {
-                    noseIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            case "Shoes":
-                shoesIV = findViewById(R.id.Shoes);
-                if (checked) {
-                    shoesIV.setVisibility(View.VISIBLE);
-                } else {
-                    shoesIV.setVisibility(View.INVISIBLE);
-                }
-                break;
-            default:
-                break;
+            }
         }
     }
 }
